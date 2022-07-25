940a337d4b46868f07893e060599cff1bfcba0d7
halilxibrahim <ibrahim_kamaci_06@hotmail.com>
2022-07-25 19:43:28 +0300

    header eklendi, icon eklendi

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,18 @@
+import Image from "next/image";
+
 function Header() {
+
+
     return (
-        <header className=''>
-            Burası header
+        <header className="">
+            <h1>Burası header</h1>
+            <Image
+              className="object-contain"
+              src="/icon.png"
+              width={200} 
+              height={100} />
         </header>
     )
 }
 
-export default Header
+export default Header;
